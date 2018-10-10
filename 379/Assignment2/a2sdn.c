85a034c5a6a698a427b0b262bb65be7e2580616a
Danissss <danis.cao@hotmail.com>
2018-10-09 22:06:13 -0600

    initial a2

---
+++ 379/Assignment2/a2sdn.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+void controller(char *argument){
+
+
+
+
+}
+
+void switch(char *argument){
+
+
+
+}
+
+
+int main(int argc, char** argv){
+	
+	
+
+
+}
