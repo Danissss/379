5e55d0b9848d951f14a5829f03be0a280da93d0c
Danissss <danis.cao@hotmail.com>
2018-10-24 20:01:07 -0600

    testing the sending frame

--- 379/Assignment2/a2sdn.cc
+++ 379/Assignment2/a2sdn.cc
@@ -5,15 +5,16 @@
 // C
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-#include <time.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <sys/resource.h>
 #include <sys/select.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 using namespace std; 
   
@@ -22,14 +23,35 @@ using namespace std;
 #define PRI 4
 #define S_LOW 0
 #define S_HIGH 1000
+#define MAXWORD 10
 
 typedef enum {ADD, QUERY, OPEN, ACK } KIND;	  // Message kinds
-// char KINDNAME[][MAXWORD]= { "STR", "INT", "FLOAT", "DONE", "ACK" };
+char KINDNAME[][MAXWORD]= { "ADD", "QUERY", "OPEN", "ACK" };
 typedef struct {int port1; int port2; char port3[10]; } PORT;
 
 typedef union { PORT ports; char switch_no[5]; } MSG;
 
 typedef struct { KIND kind; MSG msg; } FRAME;
+
+
+
+// fifo declare
+int fifo_0_1, fifo_1_1;
+
+
+
+
+//function declare
+char* RemoveDigits(char* input);
+void controller();
+void switches(char **arg, const string &input);
+MSG composeMSTR (const string &a,  int port1,  int port2, char *port3);
+void sendFrame (int fd, KIND kind, MSG *msg);
+FRAME rcvFrame (int fd);
+
+
+
+
 
 
 
@@ -53,16 +75,37 @@ void controller(){
 	// variable for select()
 
 
-	while(1){
 
+
+	
+
+	MSG    msg;
+	FRAME  frame;
+
+	while(1){
+		
+
+		// reciving fifo file
+		// controller always recive fifo_0_1 file 
+		
+		
+		frame = rcvFrame(fifo_0_1);
+		cout << "frame_port:" << frame.msg.ports.port1 << endl;
+		sendFrame(fifo_1_1, ACK, &msg);     
+		
+
+
+
+
+
+		/////////////////////////////////////
 		FD_ZERO(&readfds);
 		FD_SET(fd,&readfds);
-		sret = select(8,&readfds,NULL,NULL,NULL);
-		if(sret==0){
-			printf("sret = %d\n", sret);
-		}
+		select(8,&readfds,NULL,NULL,NULL);
 		memset((void *) buf, 0, 11);
 		ret = read(fd, (void*)buf, 10);
+		/////////////////////////////////////
+
 		// string s; s.push_back(buf); 
 
 		if(ret != -1){
@@ -91,7 +134,8 @@ void switches(char **arg, const string &input){
 	
 	int    port1;
 	int    port2;
-	string port3;
+	char  *port3;
+	port3= new char[10]; // allocate memory for pointer *port3;
 	int dest_ip_low;
 	int dest_ip_high;
 
@@ -116,31 +160,48 @@ void switches(char **arg, const string &input){
 
 
 	// composePacket and send
+	port1 = 1;
+	port2 = 2;
+	strcpy(port3,"100-110");
+
+	
+
 	FRAME frame;
 	MSG send_msg;
-	send_msg = composeMSTR(input,port1,port2,port3);
-	sendFrame(fifo_0_1,STR,&send_msg);
-	frame= rcvFrame(fifo_2_1);
+
+	// send_msg = composeMSTR(input,port1,port2,port3); // seg fault 
+	// sendFrame(fifo_0_1,OPEN,&send_msg);
+
+	// if (input.compare("sw1")==0){ 
+	// 	frame = rcvFrame(fifo_1_1); 
+	// 	cout << frame.msg.ports.port1 << endl;
+	// }
+
 
 
 	while(1){
 
 
 		// reciving frame
+		send_msg = composeMSTR(input,port1,port2,port3); // seg fault 
+		// cout<< " port1: " <<send_msg.ports.port1 << endl;
+		sendFrame(fifo_0_1,OPEN,&send_msg);
+
+		if (input.compare("sw1")==0){ 
+			frame = rcvFrame(fifo_1_1); 
+			// cout << frame.msg.ports.port1 << endl;
+		}
 
 
 
 
-
-
+		/////////////////////////////////////
 		FD_ZERO(&readfds);
 		FD_SET(fd,&readfds);
-		sret = select(8,&readfds,NULL,NULL,NULL);
-		if(sret==0){
-			printf("sret = %d\n", sret);
-		}
+		select(8,&readfds,NULL,NULL,NULL);
 		memset((void *) buf, 0, 11);
 		ret = read(fd, (void*)buf, 10);
+		/////////////////////////////////////
 		if(ret != -1){
 
 			if(strcmp(buf,"list")==10){
@@ -180,15 +241,20 @@ char* RemoveDigits(char* input)
 
 // Ref: eclass
 
-MSG composeMSTR (const char *a, const int port1, const int port2, const char *port3)
+MSG composeMSTR (const string &a,  int port1,  int port2,  char *port3)
 {
     MSG  msg;
 
     memset( (char *) &msg, 0, sizeof(msg) );
+    
     msg.ports.port1 = port1;
     msg.ports.port2 = port2;
+    // cout << port1 << endl;
+    // cout << msg.ports.port1 << endl; // at this point; the port1 get the correct value;
     strcpy(msg.ports.port3,port3);
-    strcpy(msg.switch_no,a);
+    // cout << msg.ports.port1 << endl;
+    strncpy(msg.switch_no,a.c_str(),sizeof(msg.switch_no)); // port1 value changed at this point;
+    // cout << msg.ports.port1 << endl;
     return msg;
 } 
 
@@ -227,6 +293,20 @@ int main(int argc, char** argv)
 	string argv_1_rep = "";
 	argv_1_rep = argv[1]; 
 	char *argv_1 = RemoveDigits(argv[1]);
+
+
+	//open fifo
+	if ( (fifo_0_1= open("fifo_0_1", O_RDWR)) < 0){
+        cout << "NO fifo_0_1 EXITS" << endl;
+        return 0;
+	}
+
+
+    if ( (fifo_1_1 = open("fifo_1_1", O_RDWR)) < 0){
+        cout << "NO fifo_1_1 EXITS" << endl;
+        return 0;
+    }
+
 	// cout << argv_1 << endl;
 	if (strcmp(argv_1,"cont") == 0){
 		if (argc > 3){
